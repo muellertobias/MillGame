567aada3214eb6338abeee6f19e997a5809a85b9
Tobias Müller <muellertobias789@gmail.com>
2017-10-05 15:23:21 +0200

    Structur

---
+++ MillGame/Models/GamePhases/EndPhase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MillGame.Models.GamePhases
+{
+    class EndPhase : GamePhase
+    {
+        public override bool IsMoveFinished()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override GamePhase NextPhase()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Move(Field currentField, Player currentPlayer, List<Field> fields)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MillGame/Models/GamePhases/SettingPhase.cs
@@ -0,0 +1,46 @@
+﻿using MillGame.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MillGame.Models.GamePhases
+{
+    class SettingPhase : GamePhase
+    {
+        private int Counter;
+        private const int MAX_Counter = 18;
+
+        public SettingPhase()
+        {
+            Counter = 0;
+        }
+
+        public override bool IsMoveFinished()
+        {
+            return true;
+        }
+
+        public override GamePhase NextPhase()
+        {
+            if (Counter >= MAX_Counter)
+            {
+                return new MovePhase();
+            }
+            return this;
+        }
+
+        public override bool Move(Field currentField, Player currentPlayer, List<Field> fields)
+        {
+            if (currentField.CurrentState == FieldState.Empty)
+            {
+                Counter++;
+                currentField.CurrentState = currentPlayer.Color;
+                currentPlayer.ControlledFields.Add(currentField);
+                return true;
+            }
+            return false;
+        }
+    }
+}
