64e303c6d5e09c690e030dea55ae35b00ceeb1ec
Tobias Müller <muellertobias789@gmail.com>
2017-10-07 14:48:51 +0200

    Added converters to create game board design

---
+++ MillGame/Utilities/Converters/SizeReducerConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace MillGame.Utilities.Converters
+{
+    public class SizeReducerConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values.Length != 2)
+                throw new ArgumentException();
+
+            return System.Convert.ToDouble(values[0]) + System.Convert.ToDouble(parameter) * System.Convert.ToDouble(values[1]);
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
